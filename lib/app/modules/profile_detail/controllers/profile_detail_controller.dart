968082b3026f431753df960c07eadd26dfdd44f6
mahendra panwar <deepakchandore.technorizen@gmail.com>
2024-03-05 19:27:54 +0530

    session management done both email and mobile number

--- lib/app/modules/profile_detail/controllers/profile_detail_controller.dart
+++ lib/app/modules/profile_detail/controllers/profile_detail_controller.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../../../common/common_widgets.dart';
+import '../../../data/apis/api_constants/api_key_constants.dart';
+import '../../../data/apis/api_methods/api_methods.dart';
+import '../../../data/apis/api_models/user_model.dart';
 import '../../../data/constants/string_constants.dart';
 
 class ProfileDetailController extends GetxController
@@ -16,10 +24,13 @@ class ProfileDetailController extends GetxController
   ///PROFILE VIEW MANAGE
   FocusNode focusFirstName = FocusNode();
   FocusNode focusLastName = FocusNode();
+  FocusNode focusFullName = FocusNode();
   FocusNode focusSellersAddress = FocusNode();
   final isFirstName = false.obs;
   final isLastName = false.obs;
+  final isFullName = false.obs;
   final isSellersAddress = false.obs;
+  TextEditingController fullNameController = TextEditingController();
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
   TextEditingController sellersAddressController = TextEditingController();
@@ -38,11 +49,25 @@ class ProfileDetailController extends GetxController
   TextEditingController emailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
 
+  String userId = '';
+
+  final inAsyncCall = false.obs;
+  Map<String, String> queryParameters = {};
+
+  UserData? userData;
+
+  Map<String, dynamic> bodyParams = {};
+
   @override
-  void onInit() {
+  Future<void> onInit() async {
     tabController = TabController(length: 3, vsync: this);
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    userId = sp.getString(ApiKeyConstants.userId) ?? '';
     super.onInit();
     startListener();
+    inAsyncCall.value = true;
+    await onInitWork();
+    inAsyncCall.value = false;
   }
 
   @override
@@ -60,6 +85,7 @@ class ProfileDetailController extends GetxController
   void startListener() {
     focusFirstName.addListener(onFocusChange);
     focusLastName.addListener(onFocusChange);
+    focusFullName.addListener(onFocusChange);
     focusSellersAddress.addListener(onFocusChange);
     focusDob.addListener(onFocusChange);
     focusGender.addListener(onFocusChange);
@@ -69,11 +95,58 @@ class ProfileDetailController extends GetxController
 
   void onFocusChange() {
     isFirstName.value = focusFirstName.hasFocus;
+    isFullName.value = focusFullName.hasFocus;
     isLastName.value = focusLastName.hasFocus;
     isSellersAddress.value = focusSellersAddress.hasFocus;
     isDob.value = focusDob.hasFocus;
     isGender.value = focusGender.hasFocus;
     isEmail.value = focusEmail.hasFocus;
     icPhone.value = focusPhone.hasFocus;
+  }
+
+  Future<void> onInitWork() async {
+    await getProfileApi();
+  }
+
+  Future<void> getProfileApi() async {
+    queryParameters = {
+      ApiKeyConstants.userId: userId,
+    };
+    UserModel? userModel =
+        await ApiMethods.getProfile(queryParameters: queryParameters);
+    if (userModel != null) {
+      userData = userModel.userData;
+      if (userData != null) {
+        fullNameController.text = userData!.userName ?? '';
+        sellersAddressController.text = userData!.sellerAddress ?? '';
+        dobController.text = userData!.dob ?? '';
+        genderController.text = userData!.gender ?? '';
+        emailController.text = userData!.email ?? '';
+        phoneController.text = userData!.mobile ?? '';
+      }
+      increment();
+    }
+  }
+
+  clickOnSubmitButton({String type = ''}) async {
+    if (fullNameController.text.trim().isNotEmpty &&
+        sellersAddressController.text.trim().isNotEmpty) {
+      inAsyncCall.value = true;
+      bodyParams = {
+        ApiKeyConstants.userId: userId,
+        ApiKeyConstants.userName: fullNameController.text,
+        ApiKeyConstants.email: sellersAddressController.text,
+        /*ApiKeyConstants.gender: fullNameController.text,
+        ApiKeyConstants.mobile: fullNameController.text,
+        ApiKeyConstants.dob: fullNameController.text,
+        ApiKeyConstants.whatsappNumber: fullNameController.text,*/
+      };
+      http.Response? response = await ApiMethods.updateProfile(
+          bodyParams: bodyParams, imageMap: {ApiKeyConstants.image: File('')});
+      if (response != null) {}
+      inAsyncCall.value = false;
+    } else {
+      CommonWidgets.snackBarView(title: StringConstants.allFieldsRequired);
+    }
   }
 }
